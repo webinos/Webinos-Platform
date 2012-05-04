fe1925540042f065f348d146a7d836757759d32c
Alexander Futasz <alexander.futasz@fokus.fraunhofer.de>
2012-05-04 10:27:32 +0200

    Always resume websocket connection
    Fixes stalled websocket connection in case e.g. a JSON parsing exception
    caused the try..catch handler to bypass resuming the connection.
    
    Jira-issue: WP-48

--- webinos/pzh/lib/pzh_sessionHandling.js
+++ webinos/pzh/lib/pzh_sessionHandling.js
@@ -272,9 +272,10 @@
 		try {
 			conn.pause();
 			this.processMsg(conn, data);
-			conn.resume();
 		} catch (err) {
-			log(this.sessionId, 'ERROR ', '[PZH] Exception in processing recieved message ' + err);
+			log(this.sessionId, 'ERROR', '[PZH] Exception in processing recieved message ' + err);
+		} finally {
+			conn.resume();
 		}
 	}
 
