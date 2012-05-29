f3bc84d708fa596f02acad4b2593e64273844e49
Ziran Sun <ziran.sun@samsung.com>
2012-05-29 10:56:08 +0100

    Add OS check guard for Zeroconf code
    Jira-issue: WP-72

--- webinos/pzp/lib/pzp_sessionHandling.js
+++ webinos/pzp/lib/pzp_sessionHandling.js
@@ -178,14 +178,29 @@ Pzp.prototype.update = function(callback) {
   }
   this.connectedApp();
   this.webServerState = global.states[2];
-  var ad = mdns.createAdvertisement(mdns.tcp('pzp'), global.pzpZeroconfPort);
-  ad.start();
-  
-  ad.on('error', function(err) {
-   log.error("Zeroconf PZP Advertisement error: (" + err+")");
-  });
-  
-  log.info("started pzp");                      
+
+  // Zeroconf changes
+  switch(os.type().toLowerCase()){
+    case "linux":
+      switch(os.platform().toLowerCase()){
+        case "android":
+          break;
+        case "linux":
+          var ad = mdns.createAdvertisement(mdns.tcp('pzp'), global.pzpZeroconfPort);
+          ad.start();
+          ad.on('error', function(err) {
+          log.error("Zeroconf PZP Advertisement error: (" + err+")");
+          });
+          log.info("started pzp");                      
+          break;
+      }
+      break;
+    case "darwin":
+      break;
+    case "windows_nt":
+      break;
+  }
+  //end - Zeroconf changes
 }
 
 Pzp.prototype.authenticated = function(cn, instance, callback) {
@@ -321,49 +336,62 @@ Pzp.prototype.connect = function (conn_key, conn_csr, code, callback) {
                 // state will be peer mode state
         } else if (self.mode === global.modes[1] ) { //hub mode
             self.state = global.states[0]; // not connected
+            //Zeroconf - start
+            switch(os.type().toLowerCase()){
+              case "linux":
+                switch(os.platform().toLowerCase()){
+                  case "android":
+                    break;
+                  case "linux":
+                    var browser = mdns.createBrowser(mdns.tcp('pzp'));   
+                    browser.on('error', function(err) {
+                    log.error("browser error: (" + err+")");
+                    });
+                    browser.start();
+         
+                    var msg ={};
+                    browser.on('serviceUp', function(service) {
+                    log.info("service up");
+                    msg.name = getelement(service, 'name');
+                    msg.port = getelement(service, 'port');
+                    msg.address = getelement(service, 'addresses');
+        
+                    log.info("Check ZeroConf discovery list");      
+                    var hostname = os.hostname();
+                    if(msg.name !== os.hostname()) {
+                      //Update connection - msg.name is machine name   
+                      msg.name = self.config.pzhId + "/" + msg.name + "_Pzp";
+        
+                      // Use case - Had connected to this PZP at least once 
+                      if((typeof self.connectedPzp[msg.name] !== "undefined") && self.connectedPzp[msg.name].state === global.states[0] ) {
+                        self.connectedPzp[msg.name].address = msg.address;
+                        self.connectedPzp[msg.name].port = global.pzpServerPort;
+                        var client = new pzpClient(); 
+                        client.connectOtherPZP(self, msg);
+                      }
+                      else if (typeof self.connectedPzp[msg.name] === "undefined") {
+                        log.info("new peer");   
 
-      	    var browser = mdns.createBrowser(mdns.tcp('pzp'));   
-            browser.on('error', function(err) {
-            log.error("browser error: (" + err+")");
-            });
-            browser.start();
-	     
-            var msg ={};
-            browser.on('serviceUp', function(service) {
-	        log.info("service up");
-            msg.name = getelement(service, 'name');
-            msg.port = getelement(service, 'port');
-            msg.address = getelement(service, 'addresses');
-		
-            log.info("Check ZeroConf discovery list");		
-            var hostname = os.hostname();
-            if(msg.name !== os.hostname()) {
-              //Update connection - msg.name is machine name   
-              msg.name = self.config.pzhId + "/" + msg.name + "_Pzp";
-		
-              // Use case - Had connected to this PZP at least once 
-              if((typeof self.connectedPzp[msg.name] !== "undefined") && self.connectedPzp[msg.name].state === global.states[0] ) {
-		      self.connectedPzp[msg.name].address = msg.address;
-		      //Use default port number?
-		      self.connectedPzp[msg.name].port = 8040;
-		      var client = new pzpClient(); 
-		      client.connectOtherPZP(self, msg);
-              }
-              else if (typeof self.connectedPzp[msg.name] === "undefined") {
-                log.info("new peer");	
-                msg.port = 8040;
-
-		        self.connectedPzp[msg.name] = {};
-		        self.connectedPzp[msg.name].address = msg.address;
-		        self.connectedPzp[msg.name].port    = msg.port;
-		        self.connectedPzp[msg.name].state   = global.states[1];
-		        self.mode  = global.modes[2];
-		        self.state = global.states[1];
-		        var client = new pzpClient(); 
-		        client.connectOtherPZP(self, msg);
-              }
+                        self.connectedPzp[msg.name] = {};
+                        self.connectedPzp[msg.name].address = msg.address;
+                        self.connectedPzp[msg.name].port    = global.pzpServerPort;
+                        self.connectedPzp[msg.name].state   = global.states[1];
+                        self.mode  = global.modes[2];
+                        self.state = global.states[1];
+                        var client = new pzpClient(); 
+                        client.connectOtherPZP(self, msg);
+                      }
+                    }
+                  });              
+                  break;
+                }
+                break;
+              case "darwin":
+                break;
+              case "windows_nt":
+                break;
             }
-          });
+        //end - zeroconf
         }       
       } else {
         self.mode = global.modes[1];
@@ -380,7 +408,7 @@ Pzp.prototype.connect = function (conn_key, conn_csr, code, callback) {
       }
       if(self.pzptlsServerState === global.states[0])
       {
-        log.info("Zeroconf: calling start pzptlsServer");	
+        log.info("Calling start pzptlsServer"); 
         var server = new pzpServer(); 
         server.startServer(self, function() {
         // The reason we send to PZH is because PZH acts as a point of synchronization for connecting PZP"s
