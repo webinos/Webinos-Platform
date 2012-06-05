546afd6110eeb82529b348e9c041e720d66c402a
André Paul <andre.paul@fokus.fraunhofer.de>
2012-06-05 17:20:14 +0200

    added existence tests for events api
    Jira issue: WP-103

---
+++ webinos/api/events/test/jasmine/events.spec.js
@@ -0,0 +1,53 @@
+describe('api.applauncher', function() {
+
+	var webinos = require('webinos')(__dirname);
+	
+	var RPCHandler = webinos.global.require(webinos.global.rpc.location).RPCHandler;
+	var service = webinos.global.require(webinos.global.api.events.location).Service;
+	var events = new service(RPCHandler);
+	beforeEach(function() {
+		this.addMatchers({
+			toBeFunction: function() {
+				return typeof this.actual === 'function';
+			},
+			toBeObject: function() {
+				return typeof this.actual === 'object';
+			},
+			toBeString: function() {
+				return typeof this.actual === 'string';
+			},
+			toBeNumber: function() {
+				return typeof this.actual === 'number';
+			}
+		});
+	});
+	
+	it('Events API has funtion createWebinosEvent', function() {
+		expect(events.createWebinosEvent).toBeFunction();
+	});
+	
+	it('Events API has funtion addWebinosEventListener', function() {
+		expect(events.addWebinosEventListener).toBeFunction();
+	});
+	
+	it('Events API has funtion removeWebinosEventListener', function() {
+		expect(events.removeWebinosEventListener).toBeFunction();
+	});
+	
+	it('Events API has funtion dispatchWebinosEvent', function() {
+		expect(events.WebinosEvent.dispatchWebinosEvent).toBeFunction();
+	});
+	
+	it('Events API equals http://webinos.org/api/events', function() {
+		expect(events.api).toEqual('http://webinos.org/api/events');
+	});
+	
+	it('Events API displayName is string', function() {
+		expect(events.displayName).toBeString();
+	});
+	
+	it('Events API description is string', function() {
+		expect(events.description).toBeString();
+	});
+	
+});
