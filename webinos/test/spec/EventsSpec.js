065ca97c6e7b157adbdf453f0117660cdea176e1
Alexander Futasz <alexander.futasz@fokus.fraunhofer.de>
2012-06-07 15:37:42 +0200

    Add event sending, receiving tests.
    The tests now cover creating events, onError callback if sending fails,
    succesful sending and delivering of events and receiving events.
    Added license header.
    
    Jira-issue: WP-104

--- webinos/test/spec/EventsSpec.js
+++ webinos/test/spec/EventsSpec.js
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ *  Code contributed to the webinos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2011 Alexander Futasz, Fraunhofer FOKUS
+ ******************************************************************************/
+
 describe("Events API", function() {
 	var eventsService;
 
@@ -13,16 +31,14 @@ describe("Events API", function() {
 				eventsService = service;
 			}
 		});
-	});
 
-	it("should be available from the discovery", function() {
 		waitsFor(function() {
 			return !!eventsService;
 		}, "The discovery didn't find an Events service", 5000);
+	});
 
-		runs(function() {
-			expect(eventsService).toBeDefined();
-		})
+	it("should be available from the discovery", function() {
+		expect(eventsService).toBeDefined();
 	});
 
 	it("has the necessary properties as service object", function() {
@@ -37,27 +53,27 @@ describe("Events API", function() {
 		expect(eventsService.description).toEqual(jasmine.any(String));
 		expect(eventsService).toHaveProp("icon");
 		expect(eventsService.icon).toEqual(jasmine.any(String));
-		expect(eventsService).toHaveProp("bind");
-		expect(eventsService.bind).toEqual(jasmine.any(Function));
+		expect(eventsService).toHaveProp("bindService");
+		expect(eventsService.bindService).toEqual(jasmine.any(Function));
 	});
 
 	it("can be bound", function() {
 		var bound = false;
-		
+
 		eventsService.bind({onBind: function(service) {
 			eventsService = service;
 			bound = true;
 		}});
-		
+
 		waitsFor(function() {
 			return bound;
 		}, "The service couldn't be bound", 500);
-		
+
 		runs(function() {
 			expect(bound).toEqual(true);
 		});
 	});
-	
+
 	it("has the necessary properties and functions as Events API service", function() {
 		expect(eventsService).toHaveProp("createWebinosEvent");
 		expect(eventsService.createWebinosEvent).toEqual(jasmine.any(Function));
@@ -66,30 +82,48 @@ describe("Events API", function() {
 		expect(eventsService).toHaveProp("removeWebinosEventListener");
 		expect(eventsService.removeWebinosEventListener).toEqual(jasmine.any(Function));
 	});
-	
+
 	it("can create an event", function() {
-		var ev = eventsService.createWebinosEvent();
+		var ev = eventsService.createWebinosEvent("testtype", {to: [{id: "to"}], source: {id: "from"}});
 		expect(ev).toHaveProp("type");
 		expect(ev).toHaveProp("addressing");
 		expect(ev).toHaveProp("id");
-		expect(ev).toHaveProp("inResponseTo");
-		expect(ev).toHaveProp("timestamp");
-		expect(ev).toHaveProp("expiryTimeStamp");
-		expect(ev).toHaveProp("addressingSensitive");
-		expect(ev).toHaveProp("forwarding");
-		expect(ev).toHaveProp("forwardingTimeStamp");
-		expect(ev).toHaveProp("payload");
+		expect(ev).toHaveProp("timeStamp");
 		expect(ev).toHaveProp("dispatchWebinosEvent");
-		expect(eventsService.dispatchWebinosEvent).toEqual(jasmine.any(Function));
+		expect(ev.dispatchWebinosEvent).toEqual(jasmine.any(Function));
 		expect(ev).toHaveProp("forwardWebinosEvent");
-		expect(eventsService.forwardWebinosEvent).toEqual(jasmine.any(Function));
+		expect(ev.forwardWebinosEvent).toEqual(jasmine.any(Function));
 	});
-	
-	it("can send an event", function() {
+
+	it("calls onError for dispatchWebinosEvent when no event listener registered", function() {
+		var errored = false;
+
+		var ev = eventsService.createWebinosEvent();
+		ev.dispatchWebinosEvent({
+			onError: function(event, recipient) {
+				errored = true;
+			}
+		});
+
+		waitsFor(function() {
+			return errored;
+		}, "onError callback to be called.", 200);
+
+		runs(function() {
+			expect(errored).toEqual(true);
+		});
+	});
+
+	it("can send and deliver an event", function() {
 		var sent = false;
 		var delivered = false;
-		
-		var ev = eventsService.createWebinosEvent();
+		var appId = eventsService.myAppID;
+
+		var listenerId = eventsService.addWebinosEventListener(function() {});
+
+		expect(listenerId).toEqual(jasmine.any(String));
+
+		var ev = eventsService.createWebinosEvent(null, {to: [{id: appId}], source: {id: appId}});
 		ev.dispatchWebinosEvent({
 			onSending: function(event, recipient) {
 				sent = true;
@@ -98,15 +132,45 @@ describe("Events API", function() {
 				delivered = true;
 			}
 		});
-		
+
 		waitsFor(function() {
-			return sent & delivered;
-		}, "Event couldn't be sent or delivered.", 3000);
-		
+			return sent;
+		}, "onSending callback to be called.", 1000);
+
+		waitsFor(function() {
+			return delivered;
+		}, "onDelivery callback to be called.", 3000);
+
 		runs(function() {
 			expect(sent).toEqual(true);
 			expect(delivered).toEqual(true);
 		});
+
+		eventsService.removeWebinosEventListener(listenerId);
+	});
+
+	it("can receive an event", function() {
+		var appId = eventsService.myAppID;
+		var received = false;
+
+		var listenerId = eventsService.addWebinosEventListener(function(rcvdEvt) {
+			received = true;
+		});
+
+		expect(listenerId).toEqual(jasmine.any(String));
+
+		var ev = eventsService.createWebinosEvent(null, {to: [{id: appId}], source: {id: appId}});
+		ev.dispatchWebinosEvent();
+
+		waitsFor(function() {
+			return received;
+		}, "event listener to be called.", 3000);
+
+		runs(function() {
+			expect(received).toEqual(true);
+		});
+
+		eventsService.removeWebinosEventListener(listenerId);
 	});
 
 });
