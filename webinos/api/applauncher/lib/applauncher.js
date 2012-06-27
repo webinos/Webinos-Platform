6e61e22b05a7e3e17458d40942f47b68bc792242
Alexander Futasz <alexander.futasz@fokus.fraunhofer.de>
2012-06-27 16:44:20 +0200

    Applauncher API starts default web browser.
    Change launchApplication to launch the default web browser if the
    applicationID to be launched is a http URL. Invoke errorCallback if not.
    
    Jira-issue: WP-135

--- webinos/api/applauncher/lib/applauncher.js
+++ webinos/api/applauncher/lib/applauncher.js
@@ -13,16 +13,17 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 * 
-* Copyright 2012 André Paul, Fraunhofer FOKUS
+* Copyright 2012 AndrÃ© Paul, Fraunhofer FOKUS
 ******************************************************************************/
 (function() {
+	var exec = require('child_process').exec;
 
 	/**
 	 * Webinos AppLauncher service constructor (server side).
 	 * @constructor
 	 * @param rpcHandler A handler for functions that use RPC to deliver their result.  
 	 */
-	var WebinosAppLauncherModule = function(rpcHandler) {
+	var WebinosAppLauncherModule = function(rpcHandler, params) {
 		// inherit from RPCWebinosService
 		this.base = RPCWebinosService;
 		this.base({
@@ -30,8 +31,28 @@
 			displayName:'AppLauncher API',
 			description:'The AppLauncher API for starting applications.'
 		});
+
+		this.browserExecPath = undefined;
+		if (params.browserExecPath) {
+			this.browserExecPath = params.browserExecPath;
+
+		} else {
+			var that = this;
+			if (process.platform === 'win32') {
+				exec('reg query HKCR\\http\\shell\\open\\command -ve', function(err, stdout, stderr) {
+					if (err) return;
+
+					that.browserExecPath = stdout.split('\r\n')[2].split('    ')[3];
+					if (/--/.test(that.browserExecPath)) {
+						that.browserExecPath = that.browserExecPath.split('--')[0];
+					}
+				});
+			} else if (process.platform === 'linux') {
+				this.browserExecPath = 'xdg-open';
+			}
+		}
 	};
-	
+
 	WebinosAppLauncherModule.prototype = new RPCWebinosService;
 
 	/**
@@ -44,32 +65,28 @@
 	WebinosAppLauncherModule.prototype.launchApplication = function (params, successCB, errorCB, objectRef){
 		console.log("launchApplication was invoked. AppID: " +  params.applicationID + " Parameters: " + params.params);
 		
-		var startUpLine = params.applicationID;
-		
-		var i;
-		if (typeof params.params !== 'undefined'){
-			for (i = 0; i < params.params.length; i++){
-				startUpLine = startUpLine + " " + params.params[i];
+		if (!/^http[s]?:\/{2}/.test(params.applicationID) || !this.browserExecPath) {
+			console.log("applauncher: only http[s] AppIds are allowed or no browser available.");
+			if (typeof errorCB === "function") {
+				errorCB();
 			}
+			return;
 		}
 		
-		console.log("AppLauncher trying to launch: " + startUpLine);
+		var cmdLine = this.browserExecPath.concat(' ', params.applicationID);
+		console.log("AppLauncher trying to launch: " + cmdLine);
 		
-		var exec = require('child_process').exec;
-		exec(startUpLine, function callback(error, stdout, stderr){
-		    console.log("Result: " + error + " " + stdout + " " + stderr);
-		    
-		    if (error != null){
-		    	errorCB();
-		    }
-		    else {
-		    	successCB();
-		    } 
-		    	
-		});
-		
-	};
+		exec(cmdLine, function(error, stdout, stderr){
+			console.log("Result: " + error + " " + stdout + " " + stderr);
 
+			if (error && typeof errorCB === "function") {
+				errorCB();
+				return;
+			}
+
+			successCB();
+		});
+	};
 	
 	/**
 	 * Determine whether an app is available.
@@ -78,8 +95,8 @@
 	 */
 	WebinosAppLauncherModule.prototype.appInstalled = function (params, successCB, errorCB, objectRef){
 		console.log("appInstalled was invoked");
+		errorCB();
 	};
-
 
 	exports.Service = WebinosAppLauncherModule;
 
