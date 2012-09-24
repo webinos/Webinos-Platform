777e07a52c9db0acd9b99fd3c895df20ee247897
Paddy Byers <paddy.byers@gmail.com>
2012-09-24 11:43:10 +0100

    Add app store menu options for Android
    Jira issue: WP-216

--- webinos/common/android/app/src/org/webinos/app/wrt/ui/WidgetListActivity.java
+++ webinos/common/android/app/src/org/webinos/app/wrt/ui/WidgetListActivity.java
@@ -19,17 +19,31 @@
 
 package org.webinos.app.wrt.ui;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.webinos.app.R;
 import org.webinos.app.wrt.mgr.WidgetManagerImpl;
 import org.webinos.app.wrt.mgr.WidgetManagerService;
+import org.webinos.util.AssetUtils;
+import org.webinos.util.ModuleUtils;
 
 import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -40,6 +54,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class WidgetListActivity extends ListActivity implements WidgetManagerService.LaunchListener, WidgetManagerImpl.EventListener {
+	private static final String TAG = "ListActivity";
+	private static final String STORES_FILE = "config/stores.json";
 	static final String ACTION_START = "org.webinos.wrt.START";
 	static final String ACTION_STOP = "org.webinos.wrt.STOP";
 	static final String ACTION_STOPALL = "org.webinos.wrt.STOPALL";
@@ -50,6 +66,7 @@ public class WidgetListActivity extends ListActivity implements WidgetManagerSer
 
 	static final int SCAN_MENUITEM_ID     = 0;
 	static final int CONTEXT_MENUITEM_ID  = 1;
+	static final int STORES_MENUITEM_BASE = 100;
 	static final int SCANNING_DIALOG      = 0;
 	static final int NO_WIDGETS_DIALOG    = 1;
 	static final int FOUND_WIDGETS_DIALOG = 2;
@@ -58,6 +75,7 @@ public class WidgetListActivity extends ListActivity implements WidgetManagerSer
 	private WidgetImportHelper scanner;
 	private Handler asyncRefreshHandler;
 	private String[] ids;
+	private Store[] stores;
 
 	@Override
 	public void onCreate(Bundle bundle) {
@@ -74,7 +92,14 @@ public class WidgetListActivity extends ListActivity implements WidgetManagerSer
 			mgr.addEventListener(this);
 			initList();
 		}
+		/* populate stores array (requires async fetch of icons) */
 		scanner = new WidgetImportHelper(this);
+		(new AsyncTask<Void, Void, Void>() {
+			@Override
+			protected Void doInBackground(Void... params) {
+				stores = getStores();
+				return null;
+			}}).execute();
 	}
 
 	@Override
@@ -119,16 +144,38 @@ public class WidgetListActivity extends ListActivity implements WidgetManagerSer
 		super.onCreateOptionsMenu(menu);
 		menu.setQwertyMode(true);
 		menu.add(0, SCAN_MENUITEM_ID, 0, "Scan SD card");
+		/* add any configured stores to the menu */
+		if(stores != null) {
+			for(int i = 0; i < stores.length; i++) {
+				Store store = stores[i];
+				menu.add(1, STORES_MENUITEM_BASE + i, 0, store.name).setIcon(store.icon);
+			}
+		}
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getItemId() != SCAN_MENUITEM_ID)
-			return false;
-
-		scanner.scan();
-		return true;    
+		int itemId = item.getItemId();
+		if(itemId == SCAN_MENUITEM_ID) {
+			scanner.scan();
+			return true;
+		}
+		if(itemId >= STORES_MENUITEM_BASE) {
+			Store store = stores[itemId - STORES_MENUITEM_BASE];
+			if(store != null && store.location != null) {
+				Intent storeIntent = new Intent(Intent.ACTION_VIEW);
+				storeIntent.setData(store.location);
+				storeIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				try {
+					getApplicationContext().startActivity(storeIntent);
+					return true;
+				} catch(Throwable t) {
+					Log.e(TAG, "Unable to launch store", t);
+				}
+			}
+		}
+		return false;
 	}
 
 	@Override
@@ -169,5 +216,52 @@ public class WidgetListActivity extends ListActivity implements WidgetManagerSer
 	@Override
 	public void onWidgetChanged(String installId, int event) {
 		asyncRefreshHandler.sendEmptyMessage(0);
+	}
+
+	/********************
+	 * Store handling
+	 ********************/
+
+	private static class Store {
+		String name;
+		String description;
+		Uri location;
+		Drawable icon;
+	}
+
+	private static Store readStore(Context ctx, JSONObject json) {
+		Store result = new Store();
+		try {
+			result.name = json.getString("name");
+			result.description = json.getString("description");
+			result.location = Uri.parse(json.getString("location"));
+			String iconURL = json.getString("logo");
+			try {
+				File iconFile = ModuleUtils.getResource(new URI(iconURL), true);
+				result.icon = Drawable.createFromPath(iconFile.getAbsolutePath());
+			} catch (IOException e) {
+				result.icon = ctx.getResources().getDrawable((R.drawable.webinos_icon));
+			} catch (URISyntaxException e) {
+				result.icon = ctx.getResources().getDrawable((R.drawable.webinos_icon));
+			}
+		} catch (JSONException e) {
+			result = null;
+		}
+		return result;
+	}
+	
+	private Store[] getStores() {
+		Store[] result = null;
+		try {
+			String storesData = AssetUtils.getAssetAsString(this, STORES_FILE);
+			JSONArray stores = new JSONArray(storesData);
+			int count = stores.length();
+			result = new Store[count];
+			for(int i = 0; i < count; i++)
+				result[i] = readStore(this.getApplicationContext(), stores.getJSONObject(i));
+		}
+		catch (IOException e1) {}
+		catch (JSONException e) {}
+		return result;
 	}
 }
