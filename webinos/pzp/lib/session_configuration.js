d7a7fa5073eefde2121b10560e34d687ff192b38
Andreas Botsikas <abot@epu.ntua.gr>
2012-10-04 01:10:49 +0300

    Temp fix rejectUnauthorized in session config.
    Temporary fixed the rejectUnauthorized incompatibility with node v 0.9+
    
    Jira issue: WP-396

--- webinos/pzp/lib/session_configuration.js
+++ webinos/pzp/lib/session_configuration.js
@@ -408,6 +408,7 @@ function createConfigStructure (name, type) {
   config.type        = type;
   config.name        = '';
   config.serverName  = '';
+  config.rejectUnauthorized = false; //TODO: Add configuration property for this
   return config;
 }
 
