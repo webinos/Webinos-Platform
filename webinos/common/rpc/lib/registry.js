d77551e933832e7482351fb02d771afc128cfa2b
Eelco <eelco@hailendal.org>
2012-10-08 15:35:32 +0200

    Didn't realize that this was used in the browser as well and it needed to be independend of other node modules. This commit fixes that issue. Also implement this for the id's generated in the browser context.
    Jira issue: WP-413

--- webinos/common/rpc/lib/registry.js
+++ webinos/common/rpc/lib/registry.js
@@ -17,8 +17,6 @@
  ******************************************************************************/
 
 (function () {
-    var uuid = require('node-uuid');
-    
 	/**
 	 * Registry for service objects. Used by RPC.
 	 * @constructor
@@ -38,6 +36,17 @@
 		this.objects = {};
 	};
 
+	/**
+	 * Creates a new unique identifier to be used for RPC requests and responses.
+	 * @function
+	 * @private
+	 * @param used for recursion
+	 */
+	var _getNextID = function(a) {
+	    // implementation taken from here: https://gist.github.com/982883
+        return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,_getNextID);
+	}
+
 	var _registerObject = function (callback) {
 		if (!callback) {
 			return;
@@ -49,7 +58,8 @@
 			receiverObjs = [];
 
 		// generate id
-		callback.id = uuid.v1();
+		callback.id = _getNextID();
+		
 		// verify id isn't existing already
 		var filteredRO = receiverObjs.filter(function(el, idx, array) {
 			return el.id === callback.id;
