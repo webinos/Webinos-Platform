38b863a7837d16a7273860cac200376dcfd08a1d
Cesare Cameroni <cesare.cameroni@polito.it>
2012-11-12 11:03:17 +0100

    Added DataHandlingPolicy and DataHandlingPreferences tags.
    Jira-issue: WP-563

---
+++ webinos/core/manager/policy_manager/src/core/policymanager/DataHandlingPreferences.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *  Code contributed to the webinos project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Copyright 2012 Torsec -Computer and network security group-
+ * Politecnico di Torino
+ * 
+ ******************************************************************************/
+
+#include "DataHandlingPreferences.h"
+
+DataHandlingPreferences::DataHandlingPreferences(TiXmlElement* dhpreferences){
+
+	// PolicyId attribute
+	//PolicyId = (dhpreferences->Attribute("PolicyId")!=NULL) ? dhpreferences->Attribute("PolicyId") : NULL;
+	PolicyId = dhpreferences->Attribute("PolicyId");
+
+	// AuthorizationsSet Tag
+	if(dhpreferences->FirstChild("AuthorizationsSet")){
+		authorizationsset = new AuthorizationsSet((TiXmlElement*)rule->FirstChild("AuthorizationsSet"));
+	}
+	else
+		authorizationsset = NULL;
+
+	// ObligationsSet Tag
+	if(dhpreferences->FirstChild("ObligationsSet")){
+		obligationsset = new ObligationsSet((TiXmlElement*)rule->FirstChild("ObligationsSet"));
+	}
+	else
+		obligationsset = NULL;
+}
+
+DataHandlingPreferences::~DataHandlingPreferences(){
+}
