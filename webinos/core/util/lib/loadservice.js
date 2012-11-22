6f9464ce00431768ad8f74106a73e4f3865030c4
Alexander Futász <alexander.futasz@fokus.fraunhofer.de>
2012-11-22 13:07:18 +0100

    Refactor to allow loading one or more modules
    loadServiceModules function loads an array of modules while the
    loadServiceModule function loads one module. This change refactors both
    functions to use the same code.
    
    Jira-issue: wP-573

--- webinos/core/util/lib/loadservice.js
+++ webinos/core/util/lib/loadservice.js
@@ -19,40 +19,40 @@
 var deps = require("find-dependencies")(__dirname);
 var logger = require("./logging.js")(__filename);
 
-exports.loadServiceModules = function(modules, registry, rpcHandler) {
-	var mmm = modules.map(function(m) {
+function load(mod, modDesc, registry, rpcHandler) {
+	try {
+		if (mod.Module) {
+			var ApiModule = mod.Module;
+			var m = new ApiModule(rpcHandler, modDesc.params);
+			if (!m.init) {
+				throw new Error("api module has no init function");
+			}
+
+			m.init(function register(o) {
+				registry.registerObject(o);
+			}, function unregister(o) {
+				registry.unregisterObject(o);
+			});
+		} else if (mod.Service) {
+			var Service = mod.Service;
+			var s = new Service(rpcHandler, modDesc.params);
+			registry.registerObject(s);
+		}
+	} catch (error) {
+		logger.error("Could not load module " + modDesc.name + " with message: " + error);
+	}
+}
+
+exports.loadServiceModules = function(modulesDesc, registry, rpcHandler) {
+	var mods = modulesDesc.map(function(m) {
 		return deps.global.require(deps.global.api[m.name].location);
 	});
-	for (var i=0; i<mmm.length; i++) {
-		try {
-			if (mmm[i].Module) {
-				var ApiModule = mmm[i].Module;
-				var m = new ApiModule(rpcHandler, modules[i].params);
-				if (!m.init) {
-					throw new Error("api module has no init function");
-				}
-
-				m.init(function register(o) {
-					registry.registerObject(o);
-				}, function unregister(o) {
-					registry.unregisterObject(o);
-				});
-			} else if (mmm[i].Service) {
-				var Service = mmm[i].Service;
-				var s = new Service(rpcHandler, modules[i].params);
-				registry.registerObject(s);
-			}
-		} catch (error) {
-			logger.error("Could not load module " + modules[i].name + " with message: " + error);
-		}
+	for (var i=0; i<mods.length; i++) {
+		load(mods[i], modulesDesc[i], registry, rpcHandler);
 	}
 };
 
-exports.loadServiceModule = function(m, registry, rpcHandler) {
-	try {
-		var Service = deps.global.require(deps.global.api[m.name].location).Service;
-		registry.registerObject(new Service(rpcHandler, modules[i].params));
-	} catch (error) {
-		logger.error("Could not load module " + m.name + " with message: " + error);
-	}
+exports.loadServiceModule = function(modDesc, registry, rpcHandler) {
+	var mod = deps.global.require(deps.global.api[modDesc.name].location);
+	load(mod, modDesc, registry, rpcHandler);
 };
