b83800e404ae7466a787ca2d2a6b26f319054ab7
John Lyle <john.lyle@cs.ox.ac.uk>
2013-02-19 16:40:02 +0000

    Update tests for the zone notification API
    Added the test script to the 'monitortests.html' file
    Fixed an incorrect test because 'unBindService' doesn't
    exist.
    
    Jira issue: WP-523

--- webinos/web_root/tests/api_tests/zonenotification/ZoneNotificationSpec.js
+++ webinos/web_root/tests/api_tests/zonenotification/ZoneNotificationSpec.js
@@ -32,43 +32,21 @@ describe("internal zone notification api", function () {
     }, "the service to be bound", TIMEOUT)
   }
 
-  function clearService() {
-    var bound = true;
-    runs(function () {
-      zoneService.unbindService(
-        function (s) {
-          bound = false
-        },
-        function (e) { console.log(e.message) }
-      )
-    });
-
-    waitsFor(function () {
-      return !bound
-    }, "the service to be unbound", TIMEOUT);
-
-    runs(function () {
-      zoneService = null
-    });
-  }
 
   describe("service initialization", function () {
     it("should be discoverable", function () {
       findService();
-      clearService()
     });
 
     it("should be bindable", function () {
       findService();
       bindService();
-      clearService()
     })
   });
 
   describe("contract", function () {
     beforeEach(findService);
     beforeEach(bindService);
-    afterEach(clearService);
 
     it("has the necessary generic service methods and properties", function () {
       expect(zoneService.state).toBeDefined();
