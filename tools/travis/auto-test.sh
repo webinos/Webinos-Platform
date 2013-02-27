fc8fc971671278726132f6d43c4263899c1b3ba8
John Lyle <john.lyle@cs.ox.ac.uk>
2013-02-27 17:49:29 +0000

    Travis integration
    This commit also adds another exit case in the PZP.
    
    Jira issue: WP-793
    
    Add webinos test script to Travis config
    
    This commit adds an automatic test harness which looks for
    Jasmine unit tests and executes them.  There's also a whitelist
    to select just tests that currently work.
    
    In addition, this version removes the temporary travis
    version of webinos_config and adds another test script.
    
    Jira issue: WP-793
    
    Moved the travis scripts to the tools/travis directory.
    
    To keep the root directory clean, the testing scripts are
    now in ./tools/travis.  A little neatening of code and comments
    was also included.
    
    Jira issue: WP-793

--- tools/travis/auto-test.sh
+++ tools/travis/auto-test.sh
@@ -3,6 +3,16 @@
 # This test file is designed to be run by Travis CI or by someone with a 
 # fresh install of webinos and all the dependencies.
 
+# Helper function - this will prepare the config file for use by PZH
+config_backup_file=""
+prepare_pzh_config(){
+	config_backup_file=$1".bak"
+	cp -f $1 $config_backup_file
+	sed -i 's/\(\s*"provider"\s*:\)[0-9]*,/\16080,/' $1
+	sed -i 's/\(\s*"provider_webServer"\s*:\)[0-9]*,/\16443,/' $1	
+}
+
+
 # We're going to complete all tests and then succeed or fail at the end.
 # Here's the accumulator variable
 test_failed=false
@@ -14,22 +24,24 @@ if [ $? -ne 0 ]; then
   test_failed=true;
 fi
 
-# Next, lets test starting the PZH
-mv ./webinos_config.json ./webinos_config_orig.json
-cp ./tools/travis/travis_webinos_config.json ./webinos_config.json
+# fiddle with the PZH config to use high-number ports
+prepare_pzh_config "./webinos_config.json"
+
+# Run the PZH
 node webinos_pzh.js --test
 if [ $? -ne 0 ]; then
   echo "Failed to start the PZH"
   test_failed=true;
 fi
 
-#Now we run some tests.
+# move the original config file back 
+mv -f $config_backup_file ./webinos_config.json
 
-# Policy manager
-cd ./webinos/core/manager/policy_manager/test/jasmine/
-jasmine-node .
+# Run the unit tests
+# Passing 0, enables whitelisted tests
+./tools/travis/unit-tests.sh 0
 if [ $? -ne 0 ]; then
-  echo "Failed to start the PZP"
+  echo "Failed on one or more unit tests"
   test_failed=true;
 fi
 
