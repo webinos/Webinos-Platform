15cce6f17d4f8d72e0a0af549351b8cfd75735c8
Krishna Bangalore <krishna.bangalore@in.tum.de>
2013-04-12 10:53:40 +0200

    added rpc_devicestatus.js file update for devicestatus
    jira issue: wp-872

--- webinos/core/api/devicestatus/lib/webinos.rpc.devicestatus.js
+++ webinos/core/api/devicestatus/lib/webinos.rpc.devicestatus.js
@@ -1,9 +1,9 @@
 (function () {
 	"use strict";
-
+        var devicetype;
 	var RPCWebinosService = require('webinos-jsonrpc2').RPCWebinosService;
 	var devicestatusmodule = require('./webinos.devicestatus.js').devicestatus,
-	RemoteDeviceStatusManager = function(rpcHandler) {
+	RemoteDeviceStatusManager = function(rpcHandler, params) {
 		// inherit from RPCWebinosService
 		this.base = RPCWebinosService;
 		this.base({
@@ -11,6 +11,8 @@
 			displayName: 'DeviceStatus',
 			description: 'Get information about the device status.'
 		});
+                this.params = params;
+                devicetype = params;
 	};
 	
 	RemoteDeviceStatusManager.prototype = new RPCWebinosService;
@@ -38,6 +40,10 @@
 
 	RemoteDeviceStatusManager.prototype.getPropertyValue = 
 		function (params, successCallback, errorCallback) {
+                         if (params && params[0] && params[0].aspect == "Device" && params[0].property == "type") { //If It's device type then Get it from the parameters in the webinos_config.json file
+                         successCallback(devicetype.devicetype);
+                         console.log(devicetype.devicetype);
+                         } else
 			devicestatusmodule.devicestatus.getPropertyValue(
 				function (prop) {
 					successCallback(prop);
