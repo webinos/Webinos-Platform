deb90ea93ecd794ebafe863d4888830934e96a76
Slaymaker <slay.cs.ox@gmail.com>
2013-04-18 13:56:27 +0100

    Additional policy tests in jasmine directories, with the working tests added as an additional unit test.
    This commit consists of an update to the unit test script. This update invokes the directory of working test.
    In addition there are two new directories of jasmine tests.
    The first directory is
    webinos/core/manager/policy_manager/test/jasmine.policy.tests.working
    which  contains the working tests that have been added to unit-test.sh. These tests exercise various aspects of the policy manager.
    
    The second directory is
    webinos/core/manager/policy_manager/test/jasmine.policy.tests.broken
    contains a collection of jasmine tests that highlight issues with how policies are evaluated.
    
    This commit is a set of tests only and DOES NOT fix jira issus WP-912.
    The primary error highlighted by the tests in the broken directory is that conditions containing subject matches do not behave as expected.
    
    The tests can be invoked manually by using cd to navigate the the directory and executing "jasmine-node ." from inside the directory.
    
    jira issue: WP-912

--- tools/travis/unit-tests.sh
+++ tools/travis/unit-tests.sh
@@ -53,7 +53,8 @@ run_unit_test(){
 
 #whitelist=('./webinos/core/manager/policy_manager/test/jasmine')
 whitelist=( "./webinos/core/manager/policy_manager/test/jasmine"
-	"./webinos/core/manager/widget_manager/test/jasmine" )
+	"./webinos/core/manager/widget_manager/test/jasmine"  
+	"./webinos/core/manager/policy_manager/test/jasmine.policy.tests.working" )
 #function to determine whether tests will be run on the given component
 #currently, all tests under included in the whitelist will not be filtered
 filter(){
